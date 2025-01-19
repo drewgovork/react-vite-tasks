531652e904e2a48a45fe78800c0ab87fd18c1d19
drewgovork <andrew.govorkov@gmail.com>
2025-01-19 17:56:49 +0700

    without JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-/* using JSX */
-/* import Императивный стиль */
-
+/* don't use JSX */
+import React from 'react';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
@@ -8,39 +7,71 @@ import './App.css';
 import { CurrentYear } from './assets/currentYear';
 
 export const App = () => {
-	{
-		/*const [count, setCount] = useState(0) - Императивный стиль */
-	}
 	const [count, setCount] = useState(0);
-
-	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />{' '}
-					{/*{reactLogo} и {viteLogo} - Декларативный стиль */}
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				{/*Обработчик событий - Императивный стиль */}
-				<button onClick={() => setCount((count) => count + 1)}>
-					{' '}
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-			<footer>
-				<CurrentYear /> {/* Декларативный стиль */}
-			</footer>
-		</>
+	return /*#__PURE__*/ React.createElement(
+		React.Fragment,
+		null,
+		/*#__PURE__*/ React.createElement(
+			'div',
+			null,
+			/*#__PURE__*/ React.createElement(
+				'a',
+				{
+					href: 'https://vite.dev',
+					target: '_blank',
+				},
+				/*#__PURE__*/ React.createElement('img', {
+					src: viteLogo,
+					className: 'logo',
+					alt: 'Vite logo',
+				}),
+			),
+			/*#__PURE__*/ React.createElement(
+				'a',
+				{
+					href: 'https://react.dev',
+					target: '_blank',
+				},
+				/*#__PURE__*/ React.createElement('img', {
+					src: reactLogo,
+					className: 'logo react',
+					alt: 'React logo',
+				}),
+			),
+		),
+		/*#__PURE__*/ React.createElement('h1', null, 'Vite + React'),
+		/*#__PURE__*/ React.createElement(
+			'div',
+			{
+				className: 'card',
+			},
+			/*#__PURE__*/ React.createElement(
+				'button',
+				{
+					onClick: () => setCount((count) => count + 1),
+				},
+				'count is ',
+				count,
+			),
+			/*#__PURE__*/ React.createElement(
+				'p',
+				null,
+				'Edit ',
+				/*#__PURE__*/ React.createElement('code', null, 'src/App.jsx'),
+				' and save to test HMR',
+			),
+		),
+		/*#__PURE__*/ React.createElement(
+			'p',
+			{
+				className: 'read-the-docs',
+			},
+			'Click on the Vite and React logos to learn more',
+		),
+		/*#__PURE__*/ React.createElement(
+			'footer',
+			null,
+			/*#__PURE__*/ React.createElement(CurrentYear, null),
+		),
 	);
 };
